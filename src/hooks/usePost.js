d62ba61f2136a03e5ed9c54971c0b4f65f06aed6
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-19 14:45:42 +0545

    5. use the queryClient to find the placeholder partial data from the posts already fetched to display as initial data for inidividual post

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useQuery } from 'react-query'
+import { queryClient } from '..'
 
 export const fetchPost = (postId) =>
   axios.get(`/api/posts/${postId}`).then((res) => res.data)
@@ -9,5 +10,11 @@ export default function usePost(postId) {
   // notice the use of query key, it is passed a list of keys that combined makes unique key
   // if only only keyword was used data from one post would be replaced by another
   // this way each key being different for each post won't replace other post data
-  return useQuery(['post', postId], () => fetchPost(postId))
+  return useQuery(['post', postId], () => fetchPost(postId), {
+    initialData: () => {
+      return queryClient
+        .getQueryData('posts')
+        ?.find((d) => Number(d.id) === Number(postId))
+    },
+  })
 }
