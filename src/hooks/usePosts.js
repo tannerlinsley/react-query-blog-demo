7efb4da1019da1cccd9071a060e0ad16eb7bdb95
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-19 14:00:45 +0545

    3. make use of useQuery to fetch the blog posts

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,27 +1,9 @@
 import React from 'react'
 import axios from 'axios'
+import { useQuery } from 'react-query'
 
 export default function usePosts() {
-  const [state, setState] = React.useReducer((_, action) => action, {
-    isLoading: true,
-  })
-
-  const fetch = async () => {
-    setState({ isLoading: true })
-    try {
-      const data = await axios.get('/api/posts').then((res) => res.data)
-      setState({ isSuccess: true, data })
-    } catch (error) {
-      setState({ isError: true, error })
-    }
-  }
-
-  React.useEffect(() => {
-    fetch()
-  }, [])
-
-  return {
-    ...state,
-    fetch,
-  }
+  return useQuery('posts', () =>
+    axios.get('/api/posts').then((res) => res.data)
+  )
 }
