01177268e10b64372d6d87b8f3b43b7ca65e2652
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-19 17:58:17 +0545

    10. use onMutate to reflect changes immediatedly and later re-fetch onSuccess

--- src/hooks/useCreatePost.js
+++ src/hooks/useCreatePost.js
@@ -7,6 +7,13 @@ export default function useCreatePost() {
   return useMutation(
     (values) => axios.post('/api/posts', values).then((res) => res.data),
     {
+      // to reflec the changes in the UI immediately, without waiting for request to resolve
+      onMutate: (newPostData) => {
+        queryClient.setQueryData('posts', (oldPost) => [
+          ...oldPost,
+          newPostData,
+        ])
+      },
       onSuccess: () => {
         queryClient.invalidateQueries('posts')
       },
--- src/hooks/useSavePost.js
+++ src/hooks/useSavePost.js
@@ -8,11 +8,25 @@ export default function useSavePost() {
     (values) =>
       axios.patch(`/api/posts/${values.id}`, values).then((res) => res.data),
     {
+      // to reflec the changes in the UI immediately, without waiting for request to resolve
+      onMutate: (newPostData) => {
+        queryClient.setQueryData(['post', newPostData.id], newPostData)
+      },
       // if response of PATCH is same as the one we would get from fetching complete data for individual post
       // no need to make another GET request by invalidating the query
       // rather update the data in queryClient for that post with the response from PATCH request
-      onSuccess: (data) => {
-        queryClient.setQueriesData(['post', data.id], data)
+      onSuccess: (newPostData) => {
+        queryClient.setQueriesData(['post', newPostData.id], newPostData)
+
+        // if (queryClient.getQueryData('posts')) {
+        //   queryClient.setQueryData('posts', (old) => {
+        //     return old.map((post) =>
+        //       post.id === newPostData.id ? newPostData : old
+        //     )
+        //   })
+        // } else {
+        //   queryClient.invalidateQueries('posts')
+        // }
       },
       // onSuccess: (data) => queryClient.invalidateQueries(['post', data.id]),
     }
