3cf379ad4e92978c5607e49b93c687f797e73828
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-19 16:34:22 +0545

    9. use response data from PATCH to update queryClient data for given post

--- src/hooks/useSavePost.js
+++ src/hooks/useSavePost.js
@@ -8,7 +8,13 @@ export default function useSavePost() {
     (values) =>
       axios.patch(`/api/posts/${values.id}`, values).then((res) => res.data),
     {
-      onSuccess: (data) => queryClient.invalidateQueries(['post', data.id]),
+      // if response of PATCH is same as the one we would get from fetching complete data for individual post
+      // no need to make another GET request by invalidating the query
+      // rather update the data in queryClient for that post with the response from PATCH request
+      onSuccess: (data) => {
+        queryClient.setQueriesData(['post', data.id], data)
+      },
+      // onSuccess: (data) => queryClient.invalidateQueries(['post', data.id]),
     }
   )
 }
